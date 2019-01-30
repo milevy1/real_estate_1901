4c327c7921d86385485d17517809fa2bc322a122
Matt Levy <milevy1@gmail.com>
2019-01-30 09:11:17 -0700

    Added room test setup and test for attributes

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,4 +4,14 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
 
+  def setup
+    @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_room_has_attributes
+    assert_equal :bedroom, @room.category
+    assert_equal 10, @room.length
+    assert_equal 13, @room.width
+  end
+
 end
