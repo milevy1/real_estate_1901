0c4b69b77b29d8fdf297c3daf069ab82b5cc733f
Matt Levy <milevy1@gmail.com>
2019-01-30 09:22:04 -0700

    Added house add_room test

--- test/house_test.rb
+++ test/house_test.rb
@@ -7,12 +7,21 @@ class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, 13)
+    @room_2 = Room.new(:bedroom, 11, 15)
   end
 
   def test_house_has_attributes
     assert_equal 400000, @house.price
     assert_equal "123 sugar lane", @house.address
     assert_equal [], @house.rooms
+  end
+
+  def test_house_add_room_to_rooms_attribute
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+
+    assert_equal [@room_1, @room_2], @house.rooms
   end
 
 end
